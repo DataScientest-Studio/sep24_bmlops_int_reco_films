bc7e33b1b89f647ca6b408ca472d867c1abd74cd
Carolin Stolpe <carolin.stolpe@wbstraining.de>
2024-10-15 13:19:19 +0200

    Organize split of datasets in functions

--- src/data/split_data.py
+++ src/data/split_data.py
@@ -1,47 +1,77 @@
 import os
 
+import click
 import pandas as pd
 
-from src.data.check_structure import check_existing_folder
-
-interim_data_relative_path = "../data/interim/"
-
-# Import data
-ratings = pd.read_csv("../data/raw/ratings.csv")
-
-# Format Unix-timestamp to datetime
-ratings["timestamp"] = pd.to_datetime(ratings["timestamp"], unit="s")
+from src.data.check_structure import check_existing_file, check_existing_folder
 
 
-# Split data into 4 parts
+def split_ratings(input_filepath, output_filepath):
+    """Splits ratings into 4 time intervals based on the timestamp column.
 
-ratings["time_interval"] = pd.cut(
-    ratings["timestamp"], bins=4, labels=["TI1", "TI2", "TI3", "TI4"]
-)
+    Args:
+        input_filepath (_type_): path to raw data directory
+        output_filepath (_type_): path to interim data directory
+    """
 
-# Save splitted data into csv files
+    ratings = pd.read_csv(f"{input_filepath}/ratings.csv")
+    # Format Unix-timestamp to datetime
+    ratings["timestamp"] = pd.to_datetime(ratings["timestamp"], unit="s")
 
-for i in range(1, 5):
-    df = ratings[ratings["time_interval"] == f"TI{i}"]
-    print(
-        f"Time interval {i}: {df['timestamp'].min()} - {df['timestamp'].max()} ({len(df)} ratings)"
-    )
-    if check_existing_folder(interim_data_relative_path):
-        os.makedirs(interim_data_relative_path)
-    ratings[ratings["time_interval"] == f"TI{i}"].to_csv(
-        f"{interim_data_relative_path}/ratings_TI{i}.csv", index=False
+    # Split data into 4 parts
+    ratings["time_interval"] = pd.cut(
+        ratings["timestamp"], bins=4, labels=["TI1", "TI2", "TI3", "TI4"]
     )
 
+    if check_existing_folder(output_filepath):
+        os.makedirs(output_filepath)
 
-# Do the same for the movies
-movies = pd.read_csv("../data/raw/movies.csv")
+    # Save splitted data into csv files
+    for i in range(1, 5):
+        if check_existing_file(f"{output_filepath}/ratings_TI{i}.csv"):
+            # df = ratings[ratings["time_interval"] == f"TI{i}"]
+            # print(
+            #     f"Time interval {i}: {df['timestamp'].min()} - {df['timestamp'].max()} ({len(df)} ratings)"
+            # )
+            ratings[ratings["time_interval"] == f"TI{i}"].to_csv(
+                f"{output_filepath}/ratings_TI{i}.csv", index=False
+            )
+    return
 
-# Extract the year from the title
-movies["year"] = movies["title"].str.extract("\((\d{4})\)")
-movies["year"] = pd.to_numeric(movies["year"])
 
-# Split movies into corresponding time intervals
-for index, year in enumerate(range(1995, 2020, 5)):
-    df = movies[(movies["year"] >= year) & (movies["year"] < year + 5)]
-    print(f"TI{index + 1}: Movies from {year} to {year + 5}: {len(df)}")
-    df.to_csv(f"../data/interim/movies_TI{index +1 }.csv", index=False)
+def split_movies(input_filepath, output_filepath):
+    """Splits movies into time intervals (all 5 years).
+
+    Args:
+        input_filepath (_type_): path to raw data directory
+        output_filepath (_type_): path to interim data directory
+    """
+    movies = pd.read_csv(f"{input_filepath}/movies.csv")
+
+    # Extract the year from the title
+    movies["year"] = movies["title"].str.extract("\((\d{4})\)")
+    movies["year"] = pd.to_numeric(movies["year"])
+
+    if check_existing_folder(output_filepath):
+        os.makedirs(output_filepath)
+
+    # Split movies into corresponding time intervals
+    for index, year in enumerate(range(1995, 2020, 5)):
+        print(year)
+        if check_existing_file(f"{output_filepath}/movies_TI{index + 1}.csv"):
+            df = movies[(movies["year"] >= year) & (movies["year"] <= year + 5)]
+            # print(f"TI{index + 1}: Movies from {year} to {year + 5}: {len(df)}")
+            df.to_csv(f"{output_filepath}/movies_TI{index + 1 }.csv", index=False)
+    return
+
+
+@click.command()
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
+def main(input_filepath, output_filepath):
+    split_ratings(input_filepath, output_filepath)
+    split_movies(input_filepath, output_filepath)
+
+
+if __name__ == "__main__":
+    main()
